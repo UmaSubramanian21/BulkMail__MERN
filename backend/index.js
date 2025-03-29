1b07e2a8d53a4a5123532c895f1d530e44625fe0
UmaSubramanian21 <usmcaofficial@gmail.com>
2025-03-29 16:45:45 +0530

    Completed bulkmail app

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,6 @@
 const express = require("express")
 const mongoose = require("mongoose")
-const nodemailer = require("nodemailer");
+const nodemailer = require("nodemailer")
 const cors = require("cors")
 const app = express()
 // to use the cors on express app
